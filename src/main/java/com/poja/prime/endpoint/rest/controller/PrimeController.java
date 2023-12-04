122eeaddd4990cd423cd02feb208391ac5c09309
Cedric <99885260+Cenikl@users.noreply.github.com>
2023-12-04 13:07:00 +0300

    feat: get 10 previos generated prime numbers

--- src/main/java/com/poja/prime/endpoint/rest/controller/PrimeController.java
+++ src/main/java/com/poja/prime/endpoint/rest/controller/PrimeController.java
@@ -5,12 +5,16 @@ import lombok.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 @PojaGenerated
 @RestController
 @Value
 public class PrimeController {
+
+    List<BigInteger> reviews;
     @GetMapping("/new-prime")
     public BigInteger generatePrime() {
         Random random = new Random();
@@ -19,6 +23,21 @@ public class PrimeController {
         do {
             probablePrime = new BigInteger(10000,random);
         } while (!probablePrime.isProbablePrime((int) desiredCertainty));
+        reviews.add(0,probablePrime);
         return probablePrime;
+    }
+    @GetMapping("/generated-primes")
+    public List<BigInteger> generatePrimes() {
+        List<BigInteger> tenPrime = new ArrayList<BigInteger>();
+        if(reviews.size() >= 10){;
+             for (int i = 9; i >= 0; i--) {
+                 tenPrime.add(reviews.get(i));
+             }
+        } else {
+            for(int i = reviews.size(); i > 0; i-- ){
+                tenPrime.add(reviews.get(i - 1));
+            }
+        }
+        return tenPrime;
     }
 }
